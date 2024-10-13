feba06e601ac419f2b32f3f7d46d6ac4f12737b4
Sadan009 <imamsadan009@gmail.com>
2024-10-13 20:56:01 +0530

    rest-app

---
+++ src/Components/Testimonial.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ProfilePic from "../Assets/john-doe-image.png";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonial = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Testimonial</p>
+        <h1 className="primary-heading">What They Are Saying</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+          similique doloribus eveniet tempora nostrum , voluptates optio.
+        </p>
+      </div>
+      <div className="testimonial-section-bottom">
+        <img src={ProfilePic} alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+          similique doloribus eveniet tempora nostrum , voluptates optio.
+        </p>
+        <div className="testimonials-stars-container">
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+        </div>
+        <h2>John Doe</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
